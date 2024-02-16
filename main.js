53638cfaf4ea8588d588f224325ac951f26dff70
evinsdesign <kelvinsaputra024@gmail.com>
2024-02-16 12:30:25 +0800

    L5-Event Handling

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 1,
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
       inventory: 3,
       details: ["50% cotton", "30% Wool", "20% Polyester"],
       variants: [
-        { id: 1, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 1, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
       sizes: ["S", "M", "L", "XL"],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
