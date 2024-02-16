f2700c80325919437067bf7e73610398b6770e3b
evinsdesign <kelvinsaputra024@gmail.com>
2024-02-16 13:27:20 +0800

    L6-Class & Style Binding

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart: 1,
+      cart: 0,
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
-      inventory: 3,
+      inStock: false,
       details: ["50% cotton", "30% Wool", "20% Polyester"],
       variants: [
         { id: 1, color: "green", image: "./assets/images/socks_green.jpg" },
@@ -16,6 +16,12 @@ const app = Vue.createApp({
   methods: {
     addToCart() {
       this.cart += 1;
+    },
+    // solution
+    removeFromCart() {
+      if (this.cart >= 1) {
+        this.cart -= 1;
+      }
     },
     updateImage(variantImage) {
       this.image = variantImage;
